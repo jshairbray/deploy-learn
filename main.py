4d30cde1b39a8f9c50c257536a69a95231ff53eb
Jonathan Shair <jshair@gmail.com>
2026-09-25 17:43:14 -0400

    v2: test automated build and release

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from deploy-learn v1\n"
+    return "Hello from deploy-learn v2\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
